b945cba288e2cc921b99117ebeb69e040a9100e0
harshwalia36 <harsh.p.walia@gmail.com>
2023-08-08 09:00:33 +0530

    Flask app

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
+import pickle
 
 
 from src.exception import CustomException
@@ -51,6 +52,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,params):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
